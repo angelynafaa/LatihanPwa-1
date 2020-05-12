6a52f7367c884b14f81f2ddf5ea80772c5986c86
angelynafaa <angelyna826@gmail.com>
2020-05-12 22:10:21 +0700

    instalasi service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
 // Kode service worker
+const CACHE_NAME = "codepolitan-reader-lite-v1";
+var urlsToCache = ["/", "/index.html", "/js/main.js", "/img/logo.png"];
+
+self.addEventListener("install", function(event) {
+  console.log("ServiceWorker: Menginstall..");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("ServiceWorker: Membuka cache..");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
