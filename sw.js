89c74b0890151e43c339817ec9e7f523a90ddbf7
angelynafaa <angelyna826@gmail.com>
2020-05-12 22:50:08 +0700

    add event fetch on service worker

--- sw.js
+++ sw.js
@@ -12,21 +12,27 @@ self.addEventListener("install", function(event) {
     })
   );
 });
-self.addEventListener("fetch", function(event) {
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-      caches.match(event.request).then(function(response) {
-        console.log("ServiceWorker: Menarik data: ", event.request.url);
-  
+      caches.match(event.request, {cacheName:CACHE_NAME})
+      .then(function(response) {
         if (response) {
-          console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
           return response;
         }
-  
-        console.log(
-          "ServiceWorker: Memuat aset dari server: ",
-          event.request.url
+        var fetchRequest = event.request.clone();
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200) {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+            return response;
+          }
         );
-        return fetch(event.request);
       })
     );
   });
